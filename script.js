bab21da9098c368499a7a1671c5aa13edca20bdf
amnaajmal <38943088+amnaajmal@users.noreply.github.com>
2018-10-08 13:08:53 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,43 @@
-// Write all your Javascript here!
-$.ajax({
-	url: `https://techkaro-test.herokuapp.com/api/v1/getdata`,
-	success: function (data) {
-		console.log(data);
-		document.querySelector(".card-title").innerHTML = data.info.heading;	
-		document.querySelector(".card-text").innerHTML = data.info.description;	
-		document.querySelector(".card-title1").innerHTML = data.sale.heading;	
-		document.querySelector(".card-text1").innerHTML = data.sale.description;
-
-	},
-
-	error: function (error) {
-		console.log(error);
+	function initialize(){
+		$.ajax({
+			url: `https://techkaro-test.herokuapp.com/api/v1/getdata`,
+			success: function (data) {
+				console.log(data);
+				document.querySelector(".nav-link").innerHTML=data.menuOptions[0];
+				document.querySelector(".nav1").innerHTML=data.menuOptions[1];
+				document.querySelector(".nav2").innerHTML=data.menuOptions[2];
+				document.querySelector(".nav3").innerHTML=data.menuOptions[3];
+				document.querySelector(".card-title").innerHTML=data.info.heading;
+				document.querySelector(".card-text").innerHTML=data.info.description;
+				// document.querySelector(".text-muted").innerHTML=data.date;
+				document.querySelector(".shugi").style='background-image:url(${data.imageURL})';
+				document.querySelector(".crd1").innerHTML=data.sale.heading;
+				document.querySelector(".p1").innerHTML=data.sale.description;
+			},
+			error: function (error) {
+				console.log(error);
+			}
+		});
 	}
-})
-function urdu(){
-	$.ajax({
-		url: `https://techkaro-test.herokuapp.com/api/v1/getdata?lang=ur`,
-		success: function (data) {
-			console.log(data);
-			document.querySelector(".card-title").innerHTML = data.info.heading;	
-			document.querySelector(".card-text").innerHTML = data.info.description;	
-			document.querySelector(".card-title1").innerHTML = data.sale.heading;	
-			document.querySelector(".card-text1").innerHTML = data.sale.description;
-		},
-		error: function (error) {
-			console.log(error);
-		}
-	})
-}
-function english(){
-	$.ajax({
-		url: `https://techkaro-test.herokuapp.com/api/v1/getdata?lang=en`,
-		success: function (data) {
-			console.log(data);
-			document.querySelector(".card-title").innerHTML = data.info.heading;	
-			document.querySelector(".card-text").innerHTML = data.info.description;	
-			document.querySelector(".card-title1").innerHTML = data.sale.heading;	
-			document.querySelector(".card-text1").innerHTML = data.sale.description;
-		},
-		error: function (error) {
-			console.log(error);
-		}
-	})
-}
-function chines(){
-	$.ajax({
-		url: `https://techkaro-test.herokuapp.com/api/v1/getdata?lang=zh`,
-		success: function (data) {
-			console.log(data);
-			document.querySelector(".card-title").innerHTML = data.info.heading;	
-			document.querySelector(".card-text").innerHTML = data.info.description;	
-			document.querySelector(".card-title1").innerHTML = data.sale.heading;	
-			document.querySelector(".card-text1").innerHTML = data.sale.description;
-		},
-		error: function (error) {
-			console.log(error);
-		}
-	})
-}
+	function language(locale){
+		$.ajax({
+			url: `https://techkaro-test.herokuapp.com/api/v1/getdata?lang=`+locale,
+			success: function (data) {
+				console.log(data);
+				document.querySelector(".nav-link").innerHTML=data.menuOptions[0];
+				document.querySelector(".nav1").innerHTML=data.menuOptions[1];
+				document.querySelector(".nav2").innerHTML=data.menuOptions[2];
+				document.querySelector(".nav3").innerHTML=data.menuOptions[3];
+				document.querySelector(".card-title").innerHTML=data.info.heading;
+				document.querySelector(".card-text").innerHTML=data.info.description;
+				// document.querySelector(".text-muted").innerHTML=data.date;
+				document.querySelector(".shugi").style='background-image:url(${data.imageURL})';
+				document.querySelector(".crd1").innerHTML=data.sale.heading;
+				document.querySelector(".p1").innerHTML=data.sale.description;
 
-
-
+			},
+			error: function (error) {
+				console.log(error);
+			}
+		});
+	}
